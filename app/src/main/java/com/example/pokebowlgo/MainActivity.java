ccc3d1071458fda8e84e00b265aff9a5d6524c8f
Joshua van der Poll <j.vanderpoll@fbidesign.nl>
2020-06-26 09:57:46 +0200

    Added button links & remove top title

--- app/src/main/java/com/example/pokebowlgo/MainActivity.java
+++ app/src/main/java/com/example/pokebowlgo/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.pokebowlgo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        Button createPokebowlBtn = (Button) findViewById(R.id.buttonCreatePokebowl);
+        TextView termsBtn = (TextView) findViewById(R.id.termsBtn);
+        TextView aboutBtn = (TextView) findViewById(R.id.aboutBtn);
+
+        createPokebowlBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this, TermsPoke.class));
+            }
+        });
+        termsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TermsPoke.class));
+            }
+        });
+        aboutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, WhatIsPoke.class));
+            }
+        });
     }
 }
