e2ae267af6720a8f11f052ab7f437bf6f3f992fd
Brian Loman <brianloman@hotmail.com>
2020-06-26 09:13:42 +0200

    Added Terms
    Added Terms

---
+++ app/src/main/java/com/example/pokebowlgo/TermsPoke.java
@@ -0,0 +1,14 @@
+package com.example.pokebowlgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TermsPoke extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.poke_terms);
+    }
+}
